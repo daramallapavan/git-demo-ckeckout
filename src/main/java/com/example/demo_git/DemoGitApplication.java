274121cd5234f2f65332edcff06ca0e834dc4b4a
daramallapavan <daramallapavan123@gmail.com>
2024-10-14 16:44:32 +0530

    initial comiit x

---
+++ src/main/java/com/example/demo_git/DemoGitApplication.java
@@ -0,0 +1,20 @@
+package com.example.demo_git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoGitApplication {
+	@GetMapping("/get")
+	public String get(){
+		return "get";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGitApplication.class, args);
+	}
+
+}
